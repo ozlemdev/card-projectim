deb671eb22e43887729bdb6fc810e7acde9dc6a6
ozlem <06ozlemozturk@gmail.com>
2023-08-13 01:06:11 +0300

    info

--- src/components/cardinfo/Cardİnfo.js
+++ src/components/cardinfo/Cardİnfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Cardİnfo.css";
 function Cardİnfo() {
-  return <div>Cardİnfo</div>;
+  return <div style={{ textAlign: "center", fontSize: "20px" }}>Cardİnfo</div>;
 }
 
 export default Cardİnfo;
