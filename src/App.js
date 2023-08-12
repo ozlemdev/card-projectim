ef7896dc6da6c13444509c50c45863e21258aba8
ozlem <06ozlemozturk@gmail.com>
2023-08-13 00:45:57 +0300

    card..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Card from "./components/card/Card";
 
 function App() {
-  return <div className="App">Merhaba</div>;
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
 }
 
 export default App;
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Cardİnfo from "../cardinfo/Cardİnfo";
+import CardImage from "../cardİmage/Cardİmage";
+import CardTitle from "../cardTitle/CardTitle";
 import "./card.css";
 function Card() {
   return (
     <div className="card">
-      <div className="card-body"></div>
+      <div className="card-body">
+        <CardTitle />
+        <CardImage />
+        <Cardİnfo />
+      </div>
     </div>
   );
 }
